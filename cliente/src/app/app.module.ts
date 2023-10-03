1d0522217e7944f1fbbfa8bebc242c39d9780a91
Isabella Solarte <isabella32423@hotmail.com>
2023-10-02 19:57:43 -0500

    Se empezó la vista del administrador

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdministradorComponent } from './componentes/administrador/administrador.component';
+import { DirectorComponent } from './componentes/director/director.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministradorComponent,
+    DirectorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
