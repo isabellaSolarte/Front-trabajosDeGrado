406589ee4243c52361c49b8aa43c0d3b5e2fecba
unknown <isabsolarte@.edu.co>
2023-10-10 16:22:28 -0500

    Se elminó el modulo de modelo

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { AdministradorComponent } from './componentes/administrador/administrador.component';
 import { DirectorComponent } from './componentes/director/director.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ModeloComponent } from './componentes/modelo/modelo.component';
 import { AdministradorEditarComponent } from './componentes/administrador-editar/administrador-editar.component';
 import { AdministradorCrearComponent } from './componentes/administrador-crear/administrador-crear.component';
 
@@ -14,7 +13,6 @@ import { AdministradorCrearComponent } from './componentes/administrador-crear/a
     AppComponent,
     AdministradorComponent,
     DirectorComponent,
-    ModeloComponent,
     AdministradorEditarComponent,
     AdministradorCrearComponent
   ],
