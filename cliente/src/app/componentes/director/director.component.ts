3f5983548039bd4ddac2c66ee1b4d3c0b406ab68
Isabella Solarte <isabella32423@hotmail.com>
2023-10-12 15:54:34 -0500

    Se agregó en el modelo el formato

--- cliente/src/app/componentes/director/director.component.ts
+++ cliente/src/app/componentes/director/director.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Formato } from '../modelo/FormatoADirector';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-director',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./director.component.css']
 })
 export class DirectorComponent {
+  formato:Formato={
+    _objetivos:'',
+    _con_entrega:'',
+    _realizacion:'',
+    _recursos:'',
+    _financiacion:''
+  };
+  constructor(private router: Router){
 
+  }
 }
---
+++ cliente/src/app/componentes/modelo/FormatoADirector.ts
@@ -0,0 +1,7 @@
+export interface Formato{
+    _objetivos:string;
+    _con_entrega:string;
+    _realizacion:string;
+    _recursos:string;
+    _financiacion:string;
+}
