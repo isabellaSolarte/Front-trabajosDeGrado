b0dde894647bc326292bc74381a7320347f8aed3
Isabella Solarte <isabella32423@hotmail.com>
2023-10-12 22:10:51 -0500

    Se modificó el formato

--- cliente/src/app/componentes/modelo/FormatoADirector.ts
+++ cliente/src/app/componentes/modelo/FormatoADirector.ts
@@ -1,7 +1,13 @@
 export interface Formato{
+    _id:number;
     _objetivos:string;
     _con_entrega:string;
     _realizacion:string;
     _recursos:string;
     _financiacion:string;
+    _per_programa:boolean;
+    _revision:string;
+    _recibido:string;
+    _observaciones:string;
+    _no_revision:number;
 }
