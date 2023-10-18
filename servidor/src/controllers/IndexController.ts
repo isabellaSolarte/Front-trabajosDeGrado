9128fe1bc8c54c722603c871a61c7ff7b135ec6a
Juan Ante <juan09ante@gmail.com>
2023-10-17 20:29:15 -0500

    cleaning testing area
    Cleaning the testing area to test easily

--- servidor/src/controllers/IndexController.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response} from 'express';
-
-class IndexController{
-    public index (req: Request,res: Response){
-        res.send('Hello')
-    }
-}
-
-export const indexController =new IndexController();
