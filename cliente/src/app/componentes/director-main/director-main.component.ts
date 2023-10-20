bb1ccebd686e7d3e3fd91d8fa84b06b94d5a3dd2
Isabella Solarte <isabella32423@hotmail.com>
2023-10-19 21:25:27 -0500

    Se completó la vista main del director

--- cliente/src/app/componentes/director-main/director-main.component.ts
+++ cliente/src/app/componentes/director-main/director-main.component.ts
@@ -29,4 +29,7 @@ export class DirectorMainComponent {
   irSubirFormato(){
     this.router.navigate(['/directorLlenarFormato']);
   }
+  irVerFormatos(){
+    this.router.navigate(['/directorListar']); ///directorListar
+  }
 }
