2ea5cadcba50ebecd85ca3fe3ee37ff03d175821
Isabella Solarte <isabella32423@hotmail.com>
2023-10-19 19:13:47 -0500

    Se completó el navegar del botón en subir archivos

--- cliente/src/app/componentes/director-main/director-main.component.ts
+++ cliente/src/app/componentes/director-main/director-main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProcesoList } from '../modelo/ProcesoList';
 
 @Component({
   selector: 'app-director-main',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./director-main.component.css']
 })
 export class DirectorMainComponent {
+  procesos:ProcesoList[] = [];
+  constructor(private router: Router){
+    
+  }
+  ngOnInit(): void{
+    this.getProcesos();
+  }
 
+  getProcesos(){ //listar procesos
+    
+  }
+  irSubirFormato(){
+    this.router.navigate(['/directorLlenarFormato']);
+  }
 }
