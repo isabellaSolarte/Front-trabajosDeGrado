856c05b71126f14a9eb2e072bd2d7baeef10362f
Isabella Solarte <isabella32423@hotmail.com>
2023-11-21 20:58:06 -0500

    Se creó vista de subida de anteproyecto

---
+++ cliente/src/app/componentes/director-anteproyecto/director-anteproyecto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-director-anteproyecto',
+  templateUrl: './director-anteproyecto.component.html',
+  styleUrls: ['./director-anteproyecto.component.css']
+})
+export class DirectorAnteproyectoComponent {
+
+}
